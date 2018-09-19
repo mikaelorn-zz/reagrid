8ddba24539d8ded7921ce897328d1253f7f57b3b
mikaelorn <fabianmikaelorn@gmail.com>
2018-09-19 14:44:38 +0200

    2

---
+++ src/index.js
@@ -0,0 +1,9 @@
+const React = require('react');
+const ReactDOM = require('react-dom');
+import App from './App';
+require('./index.css');
+
+ReactDOM.render(
+    <App />,
+    document.getElementById("app")
+);
