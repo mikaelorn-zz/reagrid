4660137ca08a8d9513c076494509da47d426374b
mikaelorn <fabianmikaelorn@gmail.com>
2018-09-20 11:26:31 +0200

    8

--- src/app.js
+++ src/app.js
@@ -12,7 +12,6 @@ export default class App extends React.Component {
             <Playground />
             <Footer />
         </div>
-
        );
     }
  };
---
+++ src/components/grid/grid.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Axios from 'axios';
+
+export default class Grid extends React.Component {
+
+componentDidMount() {
+    let mongodbsr = "reagrid_dbadmin:Salci8gQm@reagrid-bedug.mongodb.net/test?retryWrites=true"
+
+    Axios.get()
+}
+
+   render() {
+      return (
+          <div className="grid-container">
+
+          </div>
+      );
+   }
+}
